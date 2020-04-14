59adeae2e2d52d2e2c88ba5710c68a0980c565aa
Ethan A. Brace <ethan@braceyourself.solutions>
2020-04-14 00:20:41 +0000

    initial commit

---
+++ config/version.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'name'      =>  'Akaunting',
+
+    'code'      =>  'Argon',
+
+    'major'     =>  '2',
+
+    'minor'     =>  '0',
+
+    'patch'     =>  '8',
+
+    'build'     =>  '',
+
+    'status'    =>  'Stable',
+
+    'date'      =>  '02-April-2020',
+
+    'time'      =>  '13:00',
+
+    'zone'      =>  'GMT +3',
+
+];
